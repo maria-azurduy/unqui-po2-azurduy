3f5b044b98c1e92849d6dffa4c39a64faf798e2b
maria-azurduy <mmaria.azurduy@gmail.com>
2026-04-23 00:41:31 -0300

    avance tp3

---
+++ src/ar/edu/unq/obj2/tp3/CounterTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.obj2.tp3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CounterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
