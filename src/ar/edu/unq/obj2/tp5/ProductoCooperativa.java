a7c6405c187870180dd689d0267fb4669bc649fa
maria-azurduy <mmaria.azurduy@gmail.com>
2026-04-30 00:49:41 -0300

    pc

---
+++ src/ar/edu/unq/obj2/tp5/ProductoCooperativa.java
@@ -0,0 +1,16 @@
+package ar.edu.unq.obj2.tp5;
+
+public class ProductoCooperativa extends Producto {
+	
+	public Double descuentoIVA10() {
+		return this.getPrecio() *0.1;
+		
+	}
+	
+	public Double getPrecio() {
+		return super.getPrecio() - descuentoIVA10() ;
+	}
+	
+	
+
+}
